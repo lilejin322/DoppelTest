c2792b25d0db6453b6aa3312db17d5b50fb19af7
LI <707765315@qq.com>
2024-06-29 23:51:26 -0700

    update config

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ APOLLO_VEHICLE_back_edge_to_center = 1.043
 
 
 # DIRECTORIES =======================================
-APOLLO_ROOT = '/home/lilejin/test/BaiduApollo'
+APOLLO_ROOT = '/home/lilejin/BaiduApollo8.0'
 """Root directory of Apollo 8.0"""
 DT_ROOT = Path(__file__).parent
 """Root directory of DoppelTest"""
